99039d37d63a673f513b21cdcfe1b2c97207edb2
Franciscoobar <agustinmorobar9@gmail.com>
2024-11-30 17:58:10 -0300

    Reloj

---
+++ relojdigital.py
@@ -0,0 +1,13 @@
+import tkinter as tk ;import time
+
+ventana = tk.Tk() ;ventana.title("Reloj Digital")
+
+def actualizar_reloj(): hora_actual = time.strftime("%H:%M:%S") ; etiqueta.config(text=hora_actual) ; etiqueta.after(1000, actualizar_reloj)
+
+etiqueta = tk.Label(ventana, font=("Arial", 50), bg="black", fg= "white")
+
+etiqueta.pack(anchor="center")
+
+actualizar_reloj()
+
+ventana.mainloop()
